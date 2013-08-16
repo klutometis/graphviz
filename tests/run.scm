97846a42d770225a74d2918b5c5cebac36e29e05
Peter Danenberg <pcd@roxygen.org>
2013-08-16 11:39:32 -0700

    Add some more functions to the test.

--- tests/run.scm
+++ tests/run.scm
@@ -2,6 +2,13 @@
 
 (test-assert
  "Is the mic on?"
- (not (string-null? (with-output-to-string write-dot-preamble))))
+ (not (string-null?
+       (with-output-to-string
+         (lambda ()
+           (write-dot-preamble)
+           (write-node 'a '((label . 'not-a)))
+           (write-node 'b '((label . 'not-b)))
+           (write-edge 'a 'b '((label . 'heisenberg)))
+           (write-dot-postscript))))))
 
 (test-exit)
